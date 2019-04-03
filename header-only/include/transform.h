24b04544c04efa9f8b53e0e5f5e76adfe602512a
Ferenc Janky <fecjanky@gmail.com>
2019-04-03 06:45:22 +0200

    temp commit

--- header-only/include/transform.h
+++ header-only/include/transform.h
@@ -56,39 +56,11 @@ private:
     RangeT* r = nullptr;
 };
 
+template <typename RangeT, typename TransformationT> struct TransformationIterator;
+
 template <typename RangeT, typename TransformationT> struct TransformationSequence {
 
-    struct Iterator {
-        using iterator          = typename RangeT::iterator;
-        using traits            = std::iterator_traits<iterator>;
-        using iterator_category = typename traits::iterator_category;
-        using difference_type   = typename traits::difference_type;
-        using value_type        = std::remove_reference_t<decltype(
-            std::declval<TransformationT>()(std::declval<typename traits::value_type>()))>;
-        using reference         = std::add_lvalue_reference_t<value_type>;
-        using pointer           = std::add_pointer_t<value_type>;
-
-        Iterator(TransformationSequence& _seq, iterator _it)
-            : seq { &_seq }
-            , it { std::move(_it) }
-        {
-        }
-        decltype(auto) operator*() { return seq->tf(*it); }
-        void           operator++() { ++it; }
-        Iterator       operator++(int)
-        {
-            auto temp = *this;
-            ++(*this);
-            return temp;
-        }
-        bool operator==(const Iterator& rhs) const { return it == rhs.it; }
-        bool operator!=(const Iterator& rhs) const { return !((*this) == rhs); }
-
-        TransformationSequence* seq;
-        iterator                it;
-    };
-
-    using iterator = Iterator;
+    using iterator = TransformationIterator<RangeT, TransformationT>;
 
     TransformationSequence(RangeT l, TransformationT r)
         : range { std::move(l) }
@@ -96,86 +68,171 @@ template <typename RangeT, typename TransformationT> struct TransformationSequen
     {
     }
 
-    auto begin() { return Iterator(*this, range.begin()); }
-    auto end() { return Iterator(*this, range.end()); }
+    auto begin() { return iterator(*this, range.begin()); }
+    auto end() { return iterator(*this, range.end()); }
+
+    const TransformationT& transformation() const { return tf; }
+    TransformationT&       transformation() { return tf; }
 
 private:
     RangeT          range;
     TransformationT tf;
 };
 
+template <typename RangeT, typename TransformationT> struct TransformationIterator {
+    using iterator          = typename RangeT::iterator;
+    using traits            = std::iterator_traits<iterator>;
+    using iterator_category = typename traits::iterator_category;
+    using difference_type   = typename traits::difference_type;
+    using value_type        = std::remove_reference_t<decltype(
+        std::declval<TransformationT>()(std::declval<typename traits::value_type>()))>;
+    using reference         = std::add_lvalue_reference_t<value_type>;
+    using pointer           = std::add_pointer_t<value_type>;
+    using sequence_t        = TransformationSequence<RangeT, TransformationT>;
+
+    TransformationIterator(sequence_t& _seq, iterator _it)
+        : seq { &_seq }
+        , it { std::move(_it) }
+    {
+    }
+    decltype(auto)         operator*() { return seq->transformation()(*it); }
+    void                   operator++() { ++it; }
+    TransformationIterator operator++(int)
+    {
+        auto temp = *this;
+        ++(*this);
+        return temp;
+    }
+    bool operator==(const TransformationIterator& rhs) const { return it == rhs.it; }
+    bool operator!=(const TransformationIterator& rhs) const { return !((*this) == rhs); }
+
+    sequence_t* seq;
+    iterator    it;
+};
+
+template <typename R, typename T>
+auto operator-(TransformationIterator<R, T> const& lhs, TransformationIterator<R, T> const& rhs)
+{
+    return lhs.it - rhs.it;
+}
+
+template <typename R, typename T>
+auto operator<(TransformationIterator<R, T> const& lhs, TransformationIterator<R, T> const& rhs)
+{
+    return lhs.it < rhs.it;
+}
+
+template <typename RangeT, typename FilterPredicate> struct FilterIterator;
+
 template <typename RangeT, typename FilterPredicate> struct FilteredSequence {
 
-    struct Iterator {
-        using iterator = typename RangeT::iterator;
-
-        using traits            = std::iterator_traits<iterator>;
-        using iterator_category = std::forward_iterator_tag;
-        using difference_type   = typename traits::difference_type;
-        using value_type        = typename traits::value_type;
-        using difference_type   = typename traits::difference_type;
-        using reference         = typename traits::reference;
-        using pointer           = typename traits::pointer;
-
-        Iterator(FilteredSequence& _seq, iterator _it)
-            : seq { &_seq }
-            , it { std::move(_it) }
-        {
-            next();
-        }
-        decltype(auto) operator*() { return *it; }
-        void           operator++()
-        {
-            ++it;
-            next();
-        }
-        Iterator operator++(int)
-        {
-            auto temp = *this;
-            ++(*this);
-            return temp;
-        }
-        bool operator==(const Iterator& rhs) const { return it == rhs.it; }
-        bool operator!=(const Iterator& rhs) const { return !((*this) == rhs); }
-
-        FilteredSequence* seq;
-        iterator          it;
-
-    private:
-        void next()
-        {
-            for (; it != seq->range.end() && !seq->tf(*it); ++it)
-                ;
-        }
-    };
-
-    using iterator = Iterator;
+    using iterator = FilterIterator<RangeT, FilterPredicate>;
 
     FilteredSequence(RangeT l, FilterPredicate r)
-        : range { std::move(l) }
-        , tf { std::move(r) }
+        : _range { std::move(l) }
+        , _filter { std::move(r) }
     {
     }
 
-    auto begin() { return Iterator(*this, range.begin()); }
-    auto end() { return Iterator(*this, range.end()); }
+    auto                   begin() { return iterator(*this, _range.begin()); }
+    auto                   end() { return iterator(*this, _range.end()); }
+    FilterPredicate&       filter() { return _filter; }
+    const FilterPredicate& filter() const { return _filter; }
+    RangeT&                range() { return _range; }
+    const RangeT&          range() const { return _range; }
 
 private:
-    RangeT          range;
-    FilterPredicate tf;
+    RangeT          _range;
+    FilterPredicate _filter;
 };
 
-template <typename RangeT, typename TransformationT> auto operator*(RangeT&& r, TransformationT tf)
+template <typename RangeT, typename FilterPredicate> struct FilterIterator {
+    using iterator = typename RangeT::iterator;
+
+    using traits              = std::iterator_traits<iterator>;
+    using iterator_category   = std::forward_iterator_tag;
+    using difference_type     = typename traits::difference_type;
+    using value_type          = typename traits::value_type;
+    using difference_type     = typename traits::difference_type;
+    using reference           = typename traits::reference;
+    using pointer             = typename traits::pointer;
+    using filtered_sequence_t = FilteredSequence<RangeT, FilterPredicate>;
+
+    FilterIterator(filtered_sequence_t& _seq, iterator _it)
+        : seq { &_seq }
+        , it { std::move(_it) }
+    {
+        next();
+    }
+    decltype(auto) operator*() { return *it; }
+    void           operator++()
+    {
+        ++it;
+        next();
+    }
+    FilterIterator operator++(int)
+    {
+        auto temp = *this;
+        ++(*this);
+        return temp;
+    }
+    bool operator==(const FilterIterator& rhs) const { return it == rhs.it; }
+    bool operator!=(const FilterIterator& rhs) const { return !((*this) == rhs); }
+
+    filtered_sequence_t* seq;
+    iterator             it;
+
+private:
+    void next()
+    {
+        for (; it != seq->range().end() && !seq->filter()(*it); ++it)
+            ;
+    }
+};
+
+template <typename F> struct Transformation : public F {
+    Transformation() = default;
+    Transformation(F&& f)
+        : F(std::move(f))
+    {
+    }
+    Transformation(const F& f)
+        : F(f)
+    {
+    }
+};
+
+template <typename F> struct Filter : public F {
+    Filter() = default;
+    Filter(F&& f)
+        : F(std::move(f))
+    {
+    }
+    Filter(const F& f)
+        : F(f)
+    {
+    }
+};
+
+template <typename TransformationT> auto transform(TransformationT tf)
 {
-    using range = Range<RangeT>;
-    return TransformationSequence<range, TransformationT>(
-        range(std::forward<RangeT>(r)), std::move(tf));
+    return Transformation<TransformationT>(std::move(tf));
 }
 
-template <typename RangeT, typename FilterT> auto operator%(RangeT&& r, FilterT tf)
+template <typename FilterT> auto filter(FilterT tf) { return Filter<FilterT>(std::move(tf)); }
+
+template <typename RangeT, typename TransformationT>
+auto operator|(RangeT&& r, Transformation<TransformationT> tf)
 {
-    using range = Range<RangeT>;
-    return FilteredSequence<range, FilterT>(range(std::forward<RangeT>(r)), std::move(tf));
+    using range = Range<std::remove_reference_t<RangeT>>;
+    return TransformationSequence<std::remove_reference_t<RangeT>, TransformationT>(
+        range { std::forward<RangeT>(r) }, std::move(tf));
+}
+
+template <typename RangeT, typename FilterT> auto operator|(RangeT&& r, Filter<FilterT> tf)
+{
+    using range = Range<std::remove_reference_t<RangeT>>;
+    return FilteredSequence<range, FilterT>(range { std::forward<RangeT>(r) }, std::move(tf));
 }
 
 }
--- test/src/main.cpp
+++ test/src/main.cpp
@@ -17,37 +17,39 @@ TEST_CASE("test hello", "[hello]")
 
 TEST_CASE("transform example", "[transform]")
 {
-    std::vector<int> vec { 1, 2, 3, 4, 5, 6 };
+    // std::vector<int> vec { 1, 2, 3, 4, 5, 6 };
 
-    using transformed::operator*;
-    using transformed::operator%;
+    // using transformed::filter;
+    // using transformed::transform;
 
-    transformed::Range<decltype(vec)&> r(vec);
-    r.begin();
+    // transformed::Range<decltype(vec)&> r(vec);
+    // r.begin();
 
-    transformed::Range<decltype(vec)> r2(vec);
+    // transformed::Range<decltype(vec)> r2(vec);
 
-    auto t = vec * [](auto val) { return val * val; } * [](auto val) { return val + 1; } %
-        [](auto val) { return val % 5 == 0; } * [](auto val) { return val + 0.1; };
-    std::vector<double> res;
+    // auto t = vec | transform([](auto val) { return val * val; })
+    //    | transform([](auto val) { return val + 1; })
+    //    | filter([](auto val) { return val % 5 == 0; })
+    //    | transform([](auto val) { return val + 0.1; });
+    // std::vector<double> res;
 
-    std::copy(t.begin(), t.end(), std::back_inserter(res));
+    // std::copy(t.begin(), t.end(), std::back_inserter(res));
 
-    REQUIRE(res.size() == 2);
-    REQUIRE(res[0] == 5.1);
-    REQUIRE(res[1] == 10.1);
+    // REQUIRE(res.size() == 2);
+    // REQUIRE(res[0] == 5.1);
+    // REQUIRE(res[1] == 10.1);
 }
 
 TEST_CASE("filter example", "[transform]")
 {
     std::vector<int> vec { 1, 2, 3, 4, 5, 6 };
 
-    using transformed::operator*;
-    using transformed::operator%;
+    using transformed::filter;
+    using transformed::transform;
 
-    auto             t = vec % [](auto val) { return val >= 3; };
+    auto t = vec | transform([](auto val) { return val + 1; })
+        | filter([](auto val) { return val >= 3; });
     std::vector<int> res;
-
     std::copy(t.begin(), t.end(), std::back_inserter(res));
 
     REQUIRE(res.size() == 4);
